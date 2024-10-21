9ed759d4856d4d45362480b4323206302c5fe46b
jenberu <jemberu0970@gmail.com>
2024-10-21 10:59:27 +0300

    memes data added

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,27 +1,48 @@
 import React from "react";
+import { memesData }  from "../datas/memesData";
 import"../styles/meme.css";
-
+import { useState } from "react";
 
 const Meme = () => {
+    const [memeimage,setMemeImage]=useState({})
+    const getMemeImage = () => {
+        let randomIndex = Math.floor(Math.random() * 100);
+        const memeimage = memesData.data.memes[randomIndex];
+        setMemeImage(memeimage);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <main>
-        <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
+                
             <input 
                 type="text"
                 placeholder="Top text"
                 className="form--input"
-            />
+                    />
+               
+                        
             <input 
                 type="text"
                 placeholder="Bottom text"
                 className="form--input"
-            />
+                    />
+                  
             <button 
-                className="form--button"
+                    className="form--button"
+                    onClick={getMemeImage}
             >
                 Get a new meme image 🖼
             </button>
-        </form>
+            </form>
+            <div className="meme--image">
+                gvjhjk
+                <img src={memeimage.url}
+                    width={memeimage.width}
+                     height={memeimage.height} alt=""/>
+            </div>
     </main>
     );
 }
